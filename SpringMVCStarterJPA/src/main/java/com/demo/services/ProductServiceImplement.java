145daf006c586ff2f936a4f6bfd36ae3ff368cda
mati <mati@PC10I41>
2018-11-09 14:27:38 +0100

    .

---
+++ SpringMVCStarterJPA/src/main/java/com/demo/services/ProductServiceImplement.java
@@ -0,0 +1,22 @@
+package com.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.demo.entities.Product;
+import com.demo.repositories.ProductRepository;
+
+@Transactional
+@Service("productService")
+public class ProductServiceImplement implements ProductService {
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	@Override
+	public Product create(Product product) {
+		return productRepository.save(product);
+	}
+
+}
+
